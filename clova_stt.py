e8009488a51e6858a96f699339a42e56a7d9e7a7
cksvy203 <116814189+cksvy203@users.noreply.github.com>
2022-10-28 19:30:38 +0900

    22.10.28 소스 최종 수정본

--- clova_stt.py
+++ clova_stt.py
@@ -1,42 +1,22 @@
 import requests
-import sys
+import json
+
+# 네이버 클로바 클라이언트 정보 입력
+client_id = 'rar2f3xqjq'
+client_secret = 'GEN2mz8q0NbRbcdNySHc8D5jPJbifRANJLgqexG4'
+
+# 네이버 클로바에 사용할 언어 입력
+lang = 'Kor'
+
+# 네이버 클로바 API 주소 입력
+url = "https://naveropenapi.apigw-pub.fin-ntruss.com/recog/v1/stt?lang=" + lang
+
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": client_id,
+    "X-NCP-APIGW-API-KEY": client_secret,
+    "Content-Type": "application/octet-stream"
+}
 
 
-# 함수 정의부
-def clova_stt(clova_id, clova_secret, stype, data):
-   filename = "C:/Users/WIN10/Desktop/python_ai_speaker/hello.mp3"
-       fp = open(filename, 'rb')
-                 audio = fp.read()
-        
-    headers = {
-        "X-NCP-APIGW-API-KEY-ID": clova_id,
-        "X-NCP-APIGW-API-KEY": clova_secret,
-        "Content-Type": "application/octet-stream"
-    }
-    #네이버 클로바 API 언어
-    lang = "Kor" # 언어 코드 ( Kor )
 
-    # 클로바 음성 url
-    clova_speech_url = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + lang
-    # 클로바 음성 api 요청
-    res = requests.post(clova_speech_url, headers=headers, data=audio)
-    # 요청에 실패했다면,
-    if res.status_code != 200:
-        text=""
-        print("error! because ",  res.json())
-    else: # 성공했다면,
-    	#print("음성인식 결과 : ", res.text)
-        #print("시작위치 : ", res.text.index('{"type":"finalResult"'))
-        #print("종료위치 : ", res.text.rindex('}')+1)
-        #print("추출한 정보 : ", res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}')+1])
-        result = res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}')+1]
-        text = json.loads(result).get('value')
-
-    return text
-
-# 함수 호출부
-clova_id = "rar2f3xqjq"
-clova_secret = "GEN2mz8q0NbRbcdNySHc8D5jPJbifRANJLgqexG4"
-AUDIO_FILE = "C:/Users/WIN10/Desktop/python_ai_speaker/hello.mp3"
-text = clova_stt(clova_id, clova_secret, AUDIO_FILE)
-print(text)
+response = requests.post(url, data=audio.get_raw_data, headers=headers)
--- mic_listen.py
+++ mic_listen.py
@@ -1,27 +1,14 @@
 import speech_recognition as sr
 
-#함수 정의부
-def get_speech():
-    # 마이크에서 음성을 추출하는 객체
-    recognizer = sr.Recognizer()
+# 마이크에서 입력된 소리 신호를 텍스트로 변환
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print ("듣고있어요.")
+    audio = r.listen(source)
 
-    # 마이크 설정
-    microphone = sr.Microphone(sample_rate=16000)
+# 마이크에서 입력된 소리를 WAV 파일로 저장
+with open("test.wav", "wb") as f:
+    f.write(audio.get_wav_data())
 
-    # 마이크 소음 수치 반영
-    with microphone as source:
-        recognizer.adjust_for_ambient_noise(source)
-        print("소음 수치 반영하여 음성을 청취합니다. {}".format(recognizer.energy_threshold))
-
-        #음성 수집
-        with microphone as source:
-            print("듣고 있어요, 찬표님.")
-            result = recognizer.listen(source)
-            audio = result.get_raw_data()
-
-            return audio
-
-            #함수 호출부
-            audio = get_speech()
-            text = clova_stt("stream", audio)
-            print("음성 인식 결과 : " + text)
+    # 오디오를 raw 데이터 형식으로 불러오기
+    audio.get_raw_data()
